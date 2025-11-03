f12c3f56b98c4219bc1a4ea87f6ee07d20b5d597
POACH3 <140459864+POACH3@users.noreply.github.com>
2025-11-03 09:35:44 -0700

    Add Matchbox class

---
+++ reinforcement/MENACE/matchbox.py
@@ -0,0 +1,63 @@
+"""
+matchbox.py
+
+Defines a structure to hold learned move probabilities in a
+generic statespace with discrete moves (like a chessboard).
+"""
+
+import random
+
+class Matchbox:
+    """
+    A structure to hold learned move probabilities.
+    """
+
+    def __init__(self, state, moves, initial_beads=3):
+        self.state = state
+        self.beads = {move: initial_beads for move in moves}
+
+
+    def get_beads(self, move):
+        """
+        Gets the number of beads associated with a move. The beads represent
+        the probability of that move being selected.
+
+        Args:
+            move (string): A string representing the move.
+
+        Returns:
+            (int): The number of beads associated with the move.
+        """
+        if move in self.beads:
+            return self.beads[move]
+        else:
+            return -1
+
+
+    def set_beads(self, move, change):
+        """
+        Sets the number of beads associated with a move. This increases or decreases
+        the probability of the move being selected.
+
+        Args:
+            move (string): A string representing the move.
+            change (int): The number of beads to be added or removed.
+        """
+        if move in self.beads:
+            self.beads[move] = max(1, self.beads[move] + change)
+
+        # do some sort of error checking?
+
+
+    def select_bead(self):
+        """
+        Chooses a random bead from the matchbox.
+
+        Returns:
+             move (string): A string representing the move.
+        """
+        moves = list(self.beads.keys())
+        weights = list(self.beads.values())
+        move = random.choices(moves, weights=weights, k=1)[0]
+
+        return move
